5ff34e694ba59d689016c596e8e8a2a713aaa4a7
NLHawkins <nathanlhawkins@gmail.com>
2017-01-25 17:05:24 -0500

    create team logic complete, player logic needed

--- CU_Baseball_Stat_Modeler/Program.cs
+++ CU_Baseball_Stat_Modeler/Program.cs
@@ -16,26 +16,57 @@ namespace CU_Baseball_Stat_Modeler
             {
                 Console.WriteLine("Would you Like to Create a (T)eam, a (P)layer, or a (S)tat Sheet");
                 var objectChoice = Console.ReadLine();
-                    if (objectChoice = T)
+                if(objectChoice.ToLower() == "t")
                 {
-                    Console.WriteLine("What is the School Name of your team?");
-                    var teamName = Console.ReadLine();
-                    Console.WriteLine("What is the Mascot of your Team?");
-                    var teamMascot = Console.ReadLine();
-                    Console.WriteLine("Do you know the Rank of your time y/n");
-                    var teamRankYN = Console.ReadLine();
-                    if(teamRankYN = Y)
-                    {
-                        Console.WriteLine("What is the Rank of your Team");
-                        var teamRankString = Console.ReadLine();
-                         var teamRankInt = teamRankString.;
-                    }
-                    {
-
-                    }
+                    CreateTeam();
                 }
 
             }
+        }
+
+        private static void CreateTeam()
+        {
+                Console.WriteLine("What is the School Name of your team?");
+                var teamName = Console.ReadLine();
+                Console.WriteLine("What is the Mascot of your Team?");
+                var teamMascot = Console.ReadLine();
+                Console.WriteLine("Do you know the Rank of your time y/n");
+                var teamRankYN = Console.ReadLine();
+                var teamRankInt = new int();
+
+                if (teamRankYN.ToLower() == "y")
+                {
+                    Console.WriteLine("What is the Rank of your Team");
+                    var teamRankString = Console.ReadLine();
+                    teamRankInt = int.Parse(teamRankString);
+                }
+                else
+                {
+                    Console.WriteLine("Ok no Problem");
+                }
+                var teamId = new int();
+                for (int i = 2; i < 100; i++)
+                {
+                    teamId = i;
+                }
+
+                var userTeam = new Team
+                {
+                    SchoolName = teamName,
+                    Mascot = teamMascot,
+                    Id = teamId
+                };
+
+                if (teamRankYN == "y")
+                {
+                    userTeam.Rank = teamRankInt;
+                }           
+        }
+
+        public static void CreatePlayer()
+        {
+            Console.WriteLine("Which team [team Id] is your Player a part of?");
+            Console.WriteLine();
         }
     }
 }
