4683fb678736519af2ee466b6261db95fc05d7c7
NLHawkins <nathanlhawkins@gmail.com>
2017-01-25 13:30:47 -0500

    Add project files.

---
+++ CU_Baseball_Stat_Modeler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CU_Baseball_Stat_Modeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
